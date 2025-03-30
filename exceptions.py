20b6b64749043010210930495f3fd87fbba731f8
Kim-anthi <muindekennedy709@gmail.com>
2025-03-30 21:32:48 +0300

    advanced

---
+++ exceptions.py
@@ -0,0 +1,24 @@
+# x = 5
+# if x < 0:
+#   raise Exception('x should be positive')
+
+# x = -5
+# assert(x >= 0), 'x is not positive'
+
+try:
+  a = 5 / 0
+except:
+  print("error happened")
+  
+  
+try:
+  a = 5 / 1
+  b = a + '10'
+except ZeroDivisionError as e:
+  print(e)
+except TypeError as e:
+  print(e)
+else:
+  print("everything is fine")
+finally:
+  print("cleaning up...")
---
+++ lambda_1.py
@@ -0,0 +1,42 @@
+add10 = lambda x: x + 10
+print(add10(5))
+
+
+def add10_func(x):
+  return x + 10
+
+mult = lambda x,y: x * y
+print(mult(2,7))
+
+#sergeant
+points = [(1, 2), (15, 2), (5, -1), (10, 3)]
+
+# def sort_by_y(x):
+#   return x[1]
+points_sorted = sorted(points, key=lambda x: x[1])
+print(points)
+print(points_sorted)
+
+
+#map function
+a = [1,2,3,4,5]
+b = map(lambda x: x*2, a)
+print(list(b))
+
+c = [x*2 for x in a]
+print(c)
+
+#filter function
+a = [1,2,3,4,5,6]
+b = filter(lambda x: x%2 == 0, a)
+print(list(b))
+
+c = [x for x in a if x%2 ==0]
+print(c)
+
+
+#reduce unction
+from functools import reduce
+a = [1,2,3,4,5,6] 
+product = reduce(lambda x, y: x * y, a)
+print(product)
